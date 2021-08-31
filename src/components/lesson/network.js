7c1719565565a9c56c3679d3cfda1871ca882527
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-08-31 20:57:08 -0300

    Endpoint Lesson

--- src/components/lesson/network.js
+++ src/components/lesson/network.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  console.log('estoy en endpoint category')
-  res.json(['JavaScript', 'React', 'Redux', 'Express'])
+  console.log('estoy en endpoint lesson')
+  res.json(['Lesson 1', 'Lesson 2', 'Lesson 3', 'Lesson 4'])
 })
 
 module.exports = router
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const networkCategory = require('../components/category/network')
+const networkLesson = require('../components/lesson/network')
+
 
 router.use('/category', networkCategory)
+router.use('/lesson', networkLesson)
+
 
 module.exports = router
